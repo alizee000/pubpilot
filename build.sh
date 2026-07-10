4a9e16fde798ba9be77720180f4199eb683c3761
zeeshanait007 <zeeshanait@gmail.com>
2026-07-10 11:53:32 +0530

    Fix Vercel build script and replace broken image URLs

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Installing Flutter SDK..."
-git clone https://github.com/flutter/flutter.git -b stable
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable
+fi
 export PATH="$PATH:`pwd`/flutter/bin"
 
 echo "Enabling Web..."
@@ -10,4 +12,4 @@ echo "Getting Dependencies..."
 flutter pub get
 
 echo "Building Web App..."
-flutter build web --release
+flutter build web --release --no-tree-shake-icons
